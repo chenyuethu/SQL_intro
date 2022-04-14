3673d5a25d0aa4981b9285c15f3827e5c74bb2ce
chenyuethu <chen.yue@kellogg.northwestern.edu>
2022-04-14 19:35:22 +0000

    4-1

--- lab-4-1.sql
+++ lab-4-1.sql
@@ -1,5 +1,8 @@
 -- How many lifetime hits does Barry Bonds have?
-
+SELECT sum(hits)
+FROM players INNER JOIN stats ON players.id = stats.player_id
+WHERE first_name = 'Barry'
+AND last_name = 'Bonds'
 -- Expected result:
 -- 2935
 
