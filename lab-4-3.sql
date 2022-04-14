c93868bc7bf006d07d0a00ff0631923e672ea25f
chenyuethu <chen.yue@kellogg.northwestern.edu>
2022-04-14 19:45:25 +0000

    4-3

--- lab-4-3.sql
+++ lab-4-3.sql
@@ -1,4 +1,10 @@
 -- Who hit the most home runs in 2019, and what team did they play for?
+SELECT teams.name, first_name, last_name, home_runs
+FROM stats INNER JOIN teams ON teams.id = stats.team_id
+INNER JOIN players ON players.id = stats.player_id 
+WHERE year = 2019
+ORDER BY home_runs DESC
+LIMIT 1
 
 -- Expected result:
 --
